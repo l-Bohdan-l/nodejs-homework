baad52ebe792e08dce4c60169679796c48539605
l-Bohdan-l <bpapushoj@gmail.com>
2022-04-17 17:20:47 +0300

    add serbices folder, auth folder in controllers

---
+++ routes/api/controllers/auth/index.js
@@ -0,0 +1,15 @@
+const authService = require('../../../../services/auth');
+const {HTTP_STATUS_CODE} = require('../../../../libs/constants.js')
+
+const registration = async (req, res) => {
+
+}
+
+const login = async (req, res) => {
+
+}
+const logout = async (req, res) => {
+
+}
+
+module.exports = {registration, login, logout}
